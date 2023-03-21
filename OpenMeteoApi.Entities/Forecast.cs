5dba51be9bf1d9dd1795e85dc7afcd8c62dcf416
kah_r <kah_r@KARR>
2023-03-21 14:58:55 +0300

    Add project files.

---
+++ OpenMeteoApi.Entities/Forecast.cs
@@ -0,0 +1,15 @@
+﻿namespace OpenMeteoApi.Entities
+{
+    public class Forecast
+    {
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+        public Hourly Hourly { get; set; }
+    }
+
+    public class Hourly
+    {
+        public DateTimeOffset[] Time { get; set; }
+        public decimal[] Temperature { get; set;}
+    }
+}
